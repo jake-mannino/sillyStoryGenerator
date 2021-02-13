af50962538078fd5f83efb829a2b547804e82a6c
jake-mannino <78051933+jake-mannino@users.noreply.github.com>
2021-02-12 19:16:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was 1 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 let insertX = ['Danny Devito','Eric Andre','21 Savage'];
 let insertY = ['The surface of the sun','somewhere in florida','marianas trench'];
 let insertZ = ['folded himself 6 times','ate biscuits to death','ascended to the 8th dimension'];
@@ -22,8 +22,8 @@ let newStory = storyText;
   let zItem = randomValueFromArray(insertZ);
   
 if(document.getElementById("uk").checked) {
-    const weight = Math.round(300 * 0.714) + 'stone';
-    const temperature =  Math.round((94-32) * 5 / 9) + 'centigrade';
+    const weight = Math.round(300 * 0.714) + ' stone';
+    const temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
     newStory = newStory.replace('94 fahrenheit',temperature);
     newStory = newStory.replace('300 pounds',weight);
   }
